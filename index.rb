ca01efde3163b38debd2a727d567864b05a2bae6
alter <alter.pub@gmail.com>
2012-04-09 13:20:47 +0400

    added work with different servers

--- index.rb
+++ index.rb
@@ -10,12 +10,11 @@ set :haml, :format => :html5
 @@result = []
 @@queue  = []
 
-=begin
-m100 192.168.198.66
-Bart 195.211.130.227
-Lisa 195.211.130.227
-=end
-socket = TCPSocket.new('192.168.198.66', 50000) 
+@@servers = Hash.new()
+@@servers= { "m100" => "192.168.198.66", "Bart" => "195.211.130.227", "Lisa" => "195.211.130.227" }
+
+
+@@socket = "" # global variable for socket
 
 get '/' do
     haml :index
@@ -27,24 +26,26 @@ post '/' do
     job         = params[:job]
     server      = params[:server]
 
+    @@socket = TCPSocket.new(@@servers[server],50000)
+
     if ( version =~ /[0-9]\.[0-9]\.[0-9]{2}\.[0-9]{1,2}/ && territory =~ /[a-zA-Z_]{1,20}/ && job =~ /[a-zA-Z_]{1,20}/ && server =~ /[a-zA-Z0-9]{1,10}/ )
        arg = "--job #{job} --config #{server} --territory #{territory} --version #{version}"
-       socket.puts "\xdb#{arg}"
+       @@socket.puts "\xdb#{arg}"
     else
        arg = ""
     end
 
 
-    socket.puts "\xdcstatus"
-    while data = socket.gets()
+    @@socket.puts "\xdcstatus"
+    while data = @@socket.gets()
         if data.chomp[-1].chr == "\xde"
            break 
         end
         @@result << data
     end
 
-    socket.puts "\xdcqlist"
-    while data = socket.gets()
+    @@socket.puts "\xdcqlist"
+    while data = @@socket.gets()
         if data.chomp[-1].chr == "\xde"
            break 
         end
@@ -58,16 +59,16 @@ get '/success' do
     @@result = []
     @@queue  = []
    
-    socket.puts "\xdcstatus"
-    while data = socket.gets()
+    @@socket.puts "\xdcstatus"
+    while data = @@socket.gets()
         if data.chomp[-1].chr == "\xde"
            break 
         end
         @@result << data
     end
     
-    socket.puts "\xdcqlist" 
-    while data = socket.gets()
+    @@socket.puts "\xdcqlist" 
+    while data = @@socket.gets()
         if data.chomp[-1].chr == "\xde"
            break 
         end

f46e52ba50e8e38be82a752fe0d2d0d28d563672
alter <alter.pub@gmail.com>
2012-04-17 12:07:07 +0400

    added css styles, defaul layout and some changes in .haml files

--- index.rb
+++ index.rb
@@ -16,7 +16,28 @@ set :haml, :format => :html5
             "Lisa" => {"ip" => "195.211.130.227", "check" => 0 }
            }
 
-@@socket = "" # global variable for socket
+@@socket = "" # class variable for socket
+
+
+def getStatus()
+    @@socket.puts "\xdcstatus"
+    while data = @@socket.gets()
+        if data.chomp[-1].chr == "\xde"
+           break
+        end
+        @@result << data.chomp()
+    end
+end
+
+def getQueue()
+    @@socket.puts "\xdcqlist"
+    while data = @@socket.gets()
+        if data.chomp[-1].chr == "\xde"
+           break
+        end
+        @@queue << data
+     end
+end
 
 get '/' do
     haml :index
@@ -37,46 +58,40 @@ post '/' do
        arg = ""
     end
 
-
-    @@socket.puts "\xdcstatus"
-    while data = @@socket.gets()
-        if data.chomp[-1].chr == "\xde"
-           break 
-        end
-        @@result << data
-    end
-
-    @@socket.puts "\xdcqlist"
-    while data = @@socket.gets()
-        if data.chomp[-1].chr == "\xde"
-           break 
-        end
-        @@queue << data
-     end
-    
-    redirect '/success' 
+    redirect '/success', 307 
 end
 
 get '/success' do
+    redirect '/results', 303
+end
+
+post '/success' do
     @@result = []
     @@queue  = []
    
-    @@socket.puts "\xdcstatus"
-    while data = @@socket.gets()
-        if data.chomp[-1].chr == "\xde"
-           break 
-        end
-        @@result << data
-    end
-    
-    @@socket.puts "\xdcqlist" 
-    while data = @@socket.gets()
-        if data.chomp[-1].chr == "\xde"
-           break 
-        end
-        @@queue << data
-     end
+    getStatus()
+    getQueue()
 
- 
+    @@socket.close()
+
     haml :success, :locals => {:result => @@result, :queue => @@queue }
 end
+
+get '/results' do	
+    @@result = []
+    @@queue  = []
+
+    @@servers.each do |server,value|
+        if @@servers[server]["check"] == 1
+		    @@socket = TCPSocket.new(@@servers[server]["ip"], 50000)
+                @@result << "<h4>Server: #{@@servers[server]["ip"]} (#{server})</h4>"
+                getStatus()
+                @@queue  << "<h4>Server: #{@@servers[server]["ip"]} (#{server})</h4>"
+                getQueue()
+            @@socket.close()
+        end
+    end
+
+    haml :results, :locals => {:result => @@result, :queue => @@queue }
+end
+

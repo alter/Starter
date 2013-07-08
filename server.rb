0c74c2f29bd52a131073768b37e1e37cd87e7130
alter <alter.pub@gmail.com>
2013-07-08 17:19:14 +0400

    all

--- server.rb
+++ server.rb
@@ -4,6 +4,7 @@
 require 'socket'
 require './lib/queue.rb'
 require 'logger'
+require 'open3'
 
 host = '0.0.0.0'
 port = 50000
@@ -17,17 +18,24 @@ log.info "Logserver has been started"
 log.info ""
 $current_task = "";
 
-program_path = "/home/a1/GPT_launcher/launcher.py"
-
+PROGRAM_PATH = "/home/a1/GPT_launcher/launcher.py"
+exit_status = nil
+error = nil
 Thread.start do
     loop do
         arg = obj.pull
         if arg != nil
             $current_task = arg
+            cmd = "#{PROGRAM_PATH} #{arg}"
             log.info "Task with arguments: \"#{arg}\" has been started"
-            %x[/usr/bin/python #{program_path} #{arg}]
-            log.info "Task with arguments: \"#{arg}\" has been finished"
+            Open3.popen3(cmd) { |i,o,e,t|
+                pid = t.pid
+                exit_status = t.value 
+                error = e.read
+            }
+            log.info "Task with arguments: \"#{arg}\" has been finished with exit status #{exit_status}"
             $current_task = ""
+            File.open('error', 'w') { |file| file.write(error)}
         else
             log.info "Queue is empty. Waiting 60 seconds"
             sleep 60
@@ -37,7 +45,6 @@ end
 
 loop do
     socket = server.accept
-#    socket.set_encoding 'UTF-8' 
     Thread.start do
         port = socket.peeraddr[1]
         name = socket.peeraddr[2]
@@ -52,16 +59,12 @@ loop do
                     arg.sub!(":","")
                     arg.sub!("`","")
                     arg.sub!("&","")
+                    arg.sub!("\n","")
                     arg.sub!("|","")
                     arg.sub!("\\","")
-                    arg.sub!("\n","")
                     arg.sub!("eval","")
-#                    regex = Regexp.compile('^(.*)#.*$')
-#                    if match = s.match(regex)
-#                        arg = match.captures
-#                    end
                     log.info "Task with arguments: \"#{arg}\" has been added in queue"
-                    obj.push(arg)
+                    obj.push("#{arg}\n")
                 elsif arg[0].chr == "\xdc"
                     arg.sub!("\xdc","")
                     if arg.chomp == "qlist"
@@ -73,11 +76,15 @@ loop do
                             socket.puts("\xde")
                         end
                     elsif arg.chomp == "status"
+                        cnt = %x[wc -l ./error|awk '{print $1}'].chomp
                         if $current_task != ""
                             socket.puts($current_task)
                             socket.puts("\xde")
-                        else
+                        elsif cnt.to_i == 0
                             socket.puts("There is no running tasks")
+                            socket.puts("\xde")
+                        else 
+                            socket.puts("Error: #{exit_status}")
                             socket.puts("\xde")
                         end
                     end
